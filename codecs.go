c69a7e9bac4be2b53595f03a364dfcee68e1165b
Ali Alaee <ali@divar.ir>
2016-01-23 15:25:50 +0330

    codecs unmarshaller

--- codecs.go
+++ codecs.go
@@ -3,12 +3,16 @@ package kupak
 import (
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/api/unversioned"
+	"k8s.io/kubernetes/pkg/api/v1"
+	"k8s.io/kubernetes/pkg/apis/extensions/v1beta1"
 )
 
 type Object struct {
 	data         []byte
 	marshaller   func(interface{}) ([]byte, error)
 	unmarshaller func([]byte, interface{}) error
+	kind         string
+	value        interface{}
 }
 
 type Metadata struct {
@@ -16,26 +20,138 @@ type Metadata struct {
 	api.ObjectMeta       `json:"metadata,omitempty"`
 }
 
-func (o *Object) Metadata() (*Metadata, error) {
-	return nil
+func NewObject(data []byte,
+	marshaller func(interface{}) ([]byte, error),
+	unmarshaller func([]byte, interface{}) error) (*Object, error) {
+	obj := &Object{
+		data:         data,
+		marshaller:   marshaller,
+		unmarshaller: unmarshaller,
+	}
+	if err := obj.unmarshallToValue(); err != nil {
+		return nil, err
+	}
+	return obj, nil
 }
 
-func (o *Object) SetLabels(v map[string]string) error {
-	return nil
-}
+func (obj *Object) unmarshallToValue() error {
+	// what's the kind
+	metadata, err := obj.Metadata()
+	if err != nil {
+		return err
+	}
+	if metadata.Kind == "" {
+		obj.kind = "unknown"
+	} else {
+		obj.kind = metadata.Kind
+	}
 
-func (o *Object) SetAnnotations(v map[string]string) error {
-	return nil
-}
-
-func (o *Object) AddLabel(kv ...[]string) error {
-	return nil
-}
-
-func (o *Object) AddAnnotations(kv ...[]string) error {
+	// unmarshall to value if it's a known type
+	switch obj.kind {
+	case "ReplicationController":
+		v := v1.ReplicationController{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "Pod":
+		v := v1.Pod{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "Service":
+		v := v1.Service{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "PersistentVolume":
+		v := v1.PersistentVolume{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "Secret":
+		v := v1.Service{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "Namespace":
+		v := v1.Namespace{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "ServiceAccount":
+		v := v1.ServiceAccount{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "DaemonSet":
+		v := v1beta1.DaemonSet{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "Job":
+		v := v1beta1.Job{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "Ingress":
+		v := v1beta1.Ingress{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	case "Deployment":
+		v := v1beta1.Deployment{}
+		obj.unmarshaller(obj.data, &v)
+		obj.value = &v
+	default:
+		obj.value = nil
+	}
 	return nil
 }
 
 func (o *Object) Bytes() ([]byte, error) {
-	return nil, nil
+	return o.data, nil
+}
+
+func (o *Object) Metadata() (*Metadata, error) {
+	meta := Metadata{}
+	return &meta, o.unmarshaller(o.data, &meta)
+
+}
+
+func (o *Object) Kind() string {
+	return o.kind
+}
+
+func (o *Object) ReplicationController() *v1.ReplicationController {
+	return o.value.(*v1.ReplicationController)
+}
+
+func (o *Object) Pod() *v1.Pod {
+	return o.value.(*v1.Pod)
+}
+
+func (o *Object) Service() *v1.Service {
+	return o.value.(*v1.Service)
+}
+
+func (o *Object) PersistentVolume() *v1.PersistentVolume {
+	return o.value.(*v1.PersistentVolume)
+}
+
+func (o *Object) Secret() *v1.Secret {
+	return o.value.(*v1.Secret)
+}
+
+func (o *Object) Namespace() *v1.Namespace {
+	return o.value.(*v1.Namespace)
+}
+
+func (o *Object) ServiceAccount() *v1.ServiceAccount {
+	return o.value.(*v1.ServiceAccount)
+}
+
+func (o *Object) DaemonSet() *v1beta1.DaemonSet {
+	return o.value.(*v1beta1.DaemonSet)
+}
+
+func (o *Object) Job() *v1beta1.Job {
+	return o.value.(*v1beta1.Job)
+}
+
+func (o *Object) Ingress() *v1beta1.Ingress {
+	return o.value.(*v1beta1.Ingress)
+}
+
+func (o *Object) Deployment() *v1beta1.Deployment {
+	return o.value.(*v1beta1.Deployment)
 }
--- codecs_test.go
+++ codecs_test.go
@@ -1,14 +1,12 @@
 package kupak
 
 import (
-	"gopkg.in/yaml.v2"
 	"testing"
+
+	"github.com/ghodss/yaml"
 )
 
-func TestObjectMetadata(t *testing.T) {
-	obj := Object{
-		data: `
-apiVersion: v1
+var rc []byte = []byte(`apiVersion: v1
 kind: ReplicationController
 metadata:
   name: redis-standalone
@@ -33,22 +31,42 @@ spec:
         - name: MASTER
           value: "true"
         ports:
-        - containerPort: $(.port)
+        - containerPort: 6060
         volumeMounts:
         - mountPath: /redis-master-data
           name: data
       volumes:
         - name: data
-          emptyDir: {}
-		`,
+          emptyDir: {}`)
+
+func TestObjectMetadata(t *testing.T) {
+	obj := Object{
+		data:         rc,
 		marshaller:   yaml.Marshal,
 		unmarshaller: yaml.Unmarshal,
 	}
 	meta, err := obj.Metadata()
 	if err != nil {
+		t.Log("Metadata Error", err)
 		t.Fail()
 	}
 	if meta.Kind != "ReplicationController" {
 		t.Fail()
 	}
+}
+
+func TestReplicationController(t *testing.T) {
+	obj, err := NewObject(rc, yaml.Marshal, yaml.Unmarshal)
+	if err != nil {
+		t.Log("RC Init Error", err)
+		t.Fail()
+	}
+	rc := obj.ReplicationController()
+	if rc.Spec.Template.Labels["mode"] != "standalone" {
+		t.Log("RC Error")
+		t.Fail()
+	}
+	obj.ReplicationController().Spec.Template.Labels["mode"] = "xxx"
+	t.Log(obj.ReplicationController().Spec.Template.Labels["mode"])
+	t.Fail()
 }
