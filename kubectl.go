b41df83d34f0754a9533d044c17e11c8eaa68817
Reza Mohammadi <reza@cafebazaar.ir>
2016-02-16 17:04:41 +0330

    kubectl configuration through env vars

--- kubectl.go
+++ kubectl.go
@@ -3,6 +3,7 @@ package kupak
 import (
 	"bytes"
 	"errors"
+	"os"
 	"os/exec"
 
 	"github.com/ghodss/yaml"
@@ -10,9 +11,22 @@ import (
 
 // TODO refactor err handling and make distinct error types
 
-const (
-	kubepath = "kubectl"
+var (
+	KubePath   string
+	KubeConfig string
 )
+
+func init() {
+	if KubePath == "" {
+		KubePath = os.Getenv("KUBECTL_PATH")
+	}
+	if KubePath == "" {
+		KubePath = "kubectl" // default value
+	}
+	if KubeConfig == "" {
+		KubeConfig = os.Getenv("KUBECTL_CONFIG")
+	}
+}
 
 type kubeList struct {
 	Items []interface{} `json:"items"`
@@ -39,6 +53,9 @@ func (k *KubectlRunner) Get(namespace string, type_ string, selector string) ([]
 		type_ = "all"
 	}
 	args := []string{"-o", "json", "get", type_}
+	if KubeConfig != "" {
+		args = append(args, "--kubeconfig", KubeConfig)
+	}
 	if selector != "" {
 		args = append(args, "-l", selector)
 	}
@@ -46,7 +63,7 @@ func (k *KubectlRunner) Get(namespace string, type_ string, selector string) ([]
 		args = append(args, "--namespace", namespace)
 	}
 
-	cmd := exec.Command(kubepath, args...)
+	cmd := exec.Command(KubePath, args...)
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		return nil, errors.New(string(output))
@@ -71,11 +88,14 @@ func (k *KubectlRunner) Get(namespace string, type_ string, selector string) ([]
 
 func (k *KubectlRunner) Create(namespace string, o *Object) error {
 	args := []string{"create", "-f", "-"}
+	if KubeConfig != "" {
+		args = append(args, "--kubeconfig", KubeConfig)
+	}
 	if namespace != "" {
 		args = append([]string{"--namespace", namespace}, args...)
 	}
 
-	cmd := exec.Command(kubepath, args...)
+	cmd := exec.Command(KubePath, args...)
 
 	inBuffer, err := o.Bytes()
 	if err != nil {
