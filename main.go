ff8b5525a2a1fd534f8990d68d3f00d1fcf1ba9c
Ali Alaee <ali@divar.ir>
2016-02-28 16:45:39 +0330

    interactive installation

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 
 	"git.cafebazaar.ir/alaee/kupak/kubectl"
@@ -43,6 +45,12 @@ func main() {
 			Aliases: []string{"i"},
 			Usage:   "install the specified pak (full url or a plain name that exists in specified repo)",
 			Action:  install,
+			Flags: []cli.Flag{
+				cli.BoolFlag{
+					Name:  "interactive, i",
+					Usage: "interactive installation",
+				},
+			},
 		},
 		{
 			Name:    "list",
@@ -74,6 +82,16 @@ func main() {
 	app.Run(os.Args)
 }
 
+func stringToBool(s string) (bool, error) {
+	s = strings.ToLower(s)
+	if s == "true" || s == "yes" || s == "y" || s == "ok" {
+		return true, nil
+	} else if s == "false" || s == "n" || s == "no" {
+		return false, nil
+	}
+	return false, fmt.Errorf("can't parse \"%s\" as boolean", s)
+}
+
 func install(c *cli.Context) {
 	pakURL := c.Args().First()
 	valuesFile := c.Args().Get(1)
@@ -88,30 +106,52 @@ func install(c *cli.Context) {
 		os.Exit(-1)
 	}
 
-	// read value file
-	var valuesData []byte
-	if valuesFile == "" {
-		valuesData, err = ioutil.ReadAll(os.Stdin)
-	} else {
-		valuesData, err = ioutil.ReadFile(valuesFile)
-	}
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(-1)
-	}
-
 	values := make(map[string]interface{})
-	err = yaml.Unmarshal(valuesData, &values)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(-1)
+
+	if c.Bool("interactive") {
+		// interactive
+		for i := range p.Properties {
+			var prompt string
+			if p.Properties[i].Default == nil {
+				prompt = fmt.Sprintf("Field \"%s\" [type: %s]? ", p.Properties[i].Name, p.Properties[i].Type)
+			} else {
+				prompt = fmt.Sprintf("Field \"%s\" [type: %s, default: %v] (return for default)? ", p.Properties[i].Name, p.Properties[i].Type, p.Properties[i].Default)
+			}
+			value, err := askValue(prompt, p.Properties[i].Type, p.Properties[i].Default == nil)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+				os.Exit(-1)
+			}
+			if value != nil {
+				values[p.Properties[i].Name] = value
+			}
+		}
+	} else {
+		// read value file
+		var valuesData []byte
+		if valuesFile == "" {
+			valuesData, err = ioutil.ReadAll(os.Stdin)
+		} else {
+			valuesData, err = ioutil.ReadFile(valuesFile)
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
+		}
+		err = yaml.Unmarshal(valuesData, &values)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
+		}
 	}
 
-	_, err = pakManager.Install(p, c.GlobalString("namespace"), values)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(-1)
-	}
+	fmt.Println(values)
+
+	// _, err = pakManager.Install(p, c.GlobalString("namespace"), values)
+	// if err != nil {
+	// 	fmt.Fprintln(os.Stderr, err)
+	// 	os.Exit(-1)
+	// }
 }
 
 func paks(c *cli.Context) {
@@ -189,5 +229,44 @@ func spec(c *cli.Context) {
 			fmt.Println("   Default:", property.Default)
 		}
 
+	}
+}
+
+func askValue(prompt string, valueType string, required bool) (interface{}, error) {
+	bio := bufio.NewReader(os.Stdin)
+	var value []byte
+	var err error
+	for {
+		fmt.Printf(prompt)
+		value, _, err = bio.ReadLine()
+		if err != nil {
+			return nil, err
+		}
+		if len(value) == 0 && !required {
+			return nil, nil
+		} else if len(value) == 0 && required {
+			continue
+		}
+
+		switch valueType {
+		case "string":
+			return string(value), nil
+		case "int":
+			i, err := strconv.Atoi(string(value))
+			if err != nil {
+				fmt.Println("Bad value, try again")
+				continue
+			}
+			return i, nil
+		case "bool":
+			b, err := stringToBool(string(value))
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			return b, nil
+		default:
+			return value, nil
+		}
 	}
 }
